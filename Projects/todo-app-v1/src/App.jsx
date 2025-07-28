dfc9592858f2f4852233b2acc2e6facaf8b954de
Raees Khan <rk7523505@gmail.com>
2025-07-28 17:50:32 +0500

    add changes

--- Projects/todo-app-v1/src/App.jsx
+++ Projects/todo-app-v1/src/App.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import ProjectName from "./components/ProjectName";
 import ROw1 from "./components/ROw1";
-import Row2 from "./components/Row2";
-import Row3 from "./components/Row3";
+import TodoItems from "./components/TodoItems";
 
 const App = () => {
+  const todoItems = [
+    {
+      name: "Buy Milk",
+      dueDate: "7/8/2025",
+    },
+    {
+      name: "Go to college",
+      dueDate: "7/8/2025",
+    },
+    {
+      name: "Follow Me On Github",
+      dueDate: "Right Now",
+    },
+  ];
   return (
     <div className="todo-container">
       <ProjectName />
       <ROw1 />
-      <Row2 />
-      <Row3 />
+      <TodoItems todoItems={todoItems} />
     </div>
   );
 };
