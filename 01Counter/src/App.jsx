f23d1c81003ab4ac4ef38fcb4f7f70c7652048ae
Raees Khan <rk7523505@gmail.com>
2025-08-29 16:09:19 +0500

    Counter Project

---
+++ 01Counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>counter Project</h1>
+      <h2>ounter value: {count}</h2>
+
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrement
+      </button>
+      <br />
+      <br />
+
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Reset
+      </button>
+    </>
+  );
+}
+
+export default App;
